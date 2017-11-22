a92845f339ca8bcffd8388a20e7137bda844025e
Felipe Bernardes <bernardes.flp@gmail.com>
2017-11-22 00:35:23 -0200

    Add style for today screen and starts month screen

--- js/month.js
+++ js/month.js
@@ -14,4 +14,48 @@
   const db = { deviceId: deviceId, days: feelings };
 
   localStorage.setItem("feelings", JSON.stringify(db));
+  const mock = {"deviceId":1,"days":[{"date":"2017-11-21","feeling":"happy"},{"date":"2017-11-22","feeling":"sad"}]};
+
+  const calendar = document.querySelector('.calendar tbody');
+
+
+  const daysOfCurrentMonth = getDaysInMonth(new Date().getMonth(), new Date().getFullYear());
+
+  let firstWeekDay = new Date(daysOfCurrentMonth[0]).getDay();
+
+  daysOfCurrentMonth.forEach(day => {
+    const dayOfWeek = day.getDay();
+    const dayOfMonth = day.getDate();
+    let currentRow;
+
+    if (firstWeekDay < 7) {
+      currentRow = 0;
+    } else if (dayOfMonth > 7 && dayOfMonth <= 14) {
+      currentRow = 1;
+    } else if (dayOfMonth > 14 && dayOfMonth <= 21) {
+      currentRow = 2;
+    } else if (dayOfMonth > 21 && dayOfMonth <= 28) {
+      currentRow = 2;
+    }
+    const tds = calendar.querySelectorAll('td');
+
+    tds[dayOfWeek].innerHTML = dayOfMonth;
+  });
+
+  const calendarEntriesMarkup = mock.days.map(day => {
+    let date = luxon.DateTime.fromISO(day.date);
+    return `<td class="${day.feeling}">${date.get('day')}</td>`
+  }).join("");
+
+  //calendar.innerHTML = calendarEntriesMarkup;
+
+  function getDaysInMonth(month, year) {
+       var date = new Date(year, month, 1);
+       var days = [];
+       while (date.getMonth() === month) {
+          days.push(new Date(date));
+          date.setDate(date.getDate() + 1);
+       }
+       return days;
+  }
 })();
