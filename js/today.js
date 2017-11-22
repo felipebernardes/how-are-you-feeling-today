25e40de709a1b6f690c73bb0f99d04eb3af0fd34
amanda <amandavilelaalmeida@gmail.com>
2017-11-22 00:11:45 -0200

    Add a function to update daily feeling

--- js/today.js
+++ js/today.js
@@ -4,12 +4,28 @@
   const btnAbout = document.querySelector('.btn-about');
   const btnMonth = document.querySelector('.btn-month');
   const choices = document.querySelectorAll('.choice-button');
+  const deviceId = 01;
 
   btnAbout.addEventListener('click', (ev) => { window.location.href = 'about.html' });
   btnMonth.addEventListener('click', (ev) => { window.location.href = 'month.html' });
 
+  function updateFeeling(feeling) {
+    todayId = localStorage.getItem("todayId");
+    const url = `https://hayft-55a4a.firebaseio.com/${deviceId}/${todayId}/feeling.json`;
+    fetch(url, {
+      method: "PUT",
+      body: JSON.stringify(feeling),
+    }).then(function(response) {
+      response.json().then(function(data){
+        console.log(data);
+      });
+    })
+    .catch(function(error) {
+      console.log('An error has ocurred: ' + error.message);
+    });
+  }
+
   function addFeeling(feeling) {
-    deviceId = 01;
     const url = `https://hayft-55a4a.firebaseio.com/${deviceId}.json`;
 
     now = new Date();
@@ -21,13 +37,24 @@
         date: new Date(),
         feeling: feeling
       }),
+    }).then(function (response) {
+        response.json().then(function(data){
+          console.log(data);
+          localStorage.setItem("todayId", data.name);
+        });
+    }).catch(function(error) {
+      console.log('An error has ocurred: ' + error.message);
     });
   }
 
   choices.forEach(choice => {
     choice.addEventListener('click', (ev) => {
       feeling = choice.getAttribute("data-feeling");
-      addFeeling(feeling);
+      if (localStorage.getItem("todayId")) {
+        updateFeeling(feeling);
+      } else {
+        addFeeling(feeling);
+      }
     });
   })
 })();
