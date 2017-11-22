be5356bf5574092e631accbb8080a5ff5d7946fd
amanda <amandavilelaalmeida@gmail.com>
2017-11-21 23:07:13 -0200

    Save daily feeling at firebase

--- js/today.js
+++ js/today.js
@@ -8,9 +8,26 @@
   btnAbout.addEventListener('click', (ev) => { window.location.href = 'about.html' });
   btnMonth.addEventListener('click', (ev) => { window.location.href = 'month.html' });
 
+  function addFeeling(feeling) {
+    deviceId = 01;
+    const url = `https://hayft-55a4a.firebaseio.com/${deviceId}.json`;
+
+    now = new Date();
+    today = now.getFullYear().toString() + now.getMonth().toString() + now.getDate().toString();
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        date: new Date(),
+        feeling: feeling
+      }),
+    });
+  }
+
   choices.forEach(choice => {
     choice.addEventListener('click', (ev) => {
-      alert('mock');
+      feeling = choice.getAttribute("data-feeling");
+      addFeeling(feeling);
     });
   })
 })();
